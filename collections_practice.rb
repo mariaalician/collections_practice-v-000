c303b4208e5ecf38929e3a4d9a0ddc4d4a196b00
Maria Nunez <mariaalician@gmail.com>
2018-06-19 00:35:43 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,51 @@
+array = [1, 3, 2, 4]
+strings = ["one", "three", "four", "apple"]
 
+def sort_array_asc(array)
+  array.sort do |x,y|
+    x <=> y
+  end
+end
+
+def sort_array_desc(array)
+   array.sort do |x,y|
+    y <=> x
+  end
+end
+
+def sort_array_char_count(strings)
+   strings.sort do |x,y|
+    x <=> y
+  end
+end
+
+def swap_elements(array)
+   array[0], array[1], array[2], array[3]  = array[0], array[2], array[1]
+end
+
+def reverse_array(array)
+  reversed = array.reverse
+  reversed
+end
+
+def kesha_maker(strings)
+  strings.each do |kesha|
+    kesha[2] = "$"
+  end
+end
+
+def find_a(strings)
+  a_array = []
+  strings.collect do |starts_a|
+  apple = starts_a.start_with?("a")
+end
+  a_array << apple
+end
+
+def sum_array(array)
+  array.reduce(:+)
+end
+
+def add_s(strings)
+  
+end
